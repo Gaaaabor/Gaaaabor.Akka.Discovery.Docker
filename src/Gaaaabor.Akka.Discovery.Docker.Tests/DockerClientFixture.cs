c6c7bbc557dbda583fd5511cfd5b8706bc699b77
Bocsó Gábor <bocsgab@gmail.com>
2024-05-03 00:11:38 +0200

    Swarm support and a basic "test"

---
+++ src/Gaaaabor.Akka.Discovery.Docker.Tests/DockerClientFixture.cs
@@ -0,0 +1,37 @@
+﻿using Docker.DotNet;
+
+namespace Gaaaabor.Akka.Discovery.Docker.Tests
+{
+    public class DockerClientFixture : IDisposable
+    {
+        private bool disposedValue;
+        public DockerClient DockerClient { get; }
+
+        public DockerClientFixture()
+        {
+            var dockerClientConfiguration = new DockerClientConfiguration();
+            DockerClient = dockerClientConfiguration.CreateClient();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    DockerClient.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
